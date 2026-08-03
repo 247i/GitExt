4aecc498badfc5b500d7aee579708955f9932520
Anish T <anish.t@emerson.com>
2026-08-03 22:47:38 +0530

    Update .bat

--- .bat
+++ .bat
@@ -6,13 +6,20 @@ set "Git_FOLDER="
 :: Clone the repository into the specified subfolder
 git clone %Git_REPO% %Git_FOLDER%
 
-
 :: Define the repository URL and the target subfolder name
 set "WinMerge_REPO=https://github.com/247i/WinMerge.git"
 set "WinMerge_FOLDER="
 
 :: Clone the repository into the specified subfolder
 git clone %WinMerge_REPO% %WinMerge_FOLDER%
+
+:: Define the repository URL and the target subfolder name
+set "WinSCP_REPO=https://github.com/247i/WinMerge.git"
+set "WinSCP_FOLDER="
+
+:: Clone the repository into the specified subfolder
+git clone %WinSCP_REPO% %WinSCP_FOLDER%
+
 
 
 :: Get the VSCode portable into vscode folder manually
