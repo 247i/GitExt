e1318bdb2d2b13fbd7b4a70ffceac7a9537c4c46
Anish T <anish.t@emerson.com>
2026-08-03 23:03:33 +0530

    cleanup

--- .bat
+++ .bat
@@ -2,26 +2,29 @@
 :: Define the repository URL and the target subfolder name
 set "Git_REPO=https://github.com/247i/Git"
 set "Git_FOLDER="
-
 :: Clone the repository into the specified subfolder
 git clone %Git_REPO% %Git_FOLDER%
 
 :: Define the repository URL and the target subfolder name
 set "WinMerge_REPO=https://github.com/247i/WinMerge.git"
 set "WinMerge_FOLDER="
-
 :: Clone the repository into the specified subfolder
 git clone %WinMerge_REPO% %WinMerge_FOLDER%
 
 :: Define the repository URL and the target subfolder name
 set "WinSCP_REPO=https://github.com/247i/WinMerge.git"
 set "WinSCP_FOLDER="
-
 :: Clone the repository into the specified subfolder
 git clone %WinSCP_REPO% %WinSCP_FOLDER%
 
-
+:: Define the repository URL and the target subfolder name
+set "PuTTY_REPO=https://github.com/247i/PuTTY.git"
+set "PuTTY_FOLDER="
+:: Clone the repository into the specified subfolder
+git clone %PuTTY_REPO% %PuTTY_FOLDER%
 
 :: Get the VSCode portable into vscode folder manually
+
 :: Get the VSCodium portable into vscodium folder manually
+
 pause
